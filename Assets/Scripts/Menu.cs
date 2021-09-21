ab0753308cadc7afd11b3da2dd62dd27d38d8294
Muhammad Hafizh Azzasafah <85096618+Akanerd@users.noreply.github.com>
2021-09-22 00:25:27 +0700

    add features level

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Scene_1()
+    {
+        SceneManager.LoadScene("belajar-AngryBirds");
+    }
+
+    public void Scene_2()
+    {
+        SceneManager.LoadScene("belajar-AngryBirds 1");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
